77e3ce3ec40346f8e0e45febbf4cf23c6d27051e
MyNewName <gunha1215@gmail.com>
2025-09-02 10:45:13 +0900

    tt

---
+++ TIL/250902/practice/articles/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    path('search_result', views.search_result, name='search_result'),
+    path('detail/<int:num>', views.detail, name = 'detail'),
+]
---
+++ TIL/250902/practice/articles/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'articles/index.html')
+
+
+def search(request):
+    return render(request, 'articles/search.html')
+
+def search_result(request):
+    request_data = request.GET
+    context = {
+        'num' : request_data.get('num')
+    }
+
+    return render(request, 'articles/search.html', context)
+
+
+def detail(request, num):
+    context = {
+        'num' : num
+    }
+    return render(request, 'articles/detail.html', context)
