d3d1838b290c1cc566f71959ad1ab3027c4d05c8
MyNewName <gunha1215@gmail.com>
2025-09-03 10:50:13 +0900

    tt

---
+++ TIL/250903/django/04. django_orm/skeleton_codes/articles/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from .models import Article
+
+# Create your views here.
+def articles(request):
+    articles = Article.objects.all()
+    context = {
+        'articles' : articles
+    }
+    return render(request, 'articles/articles.html', context)
+
+
+def index(request):
+    return render(request, 'articles/index.html')
+
+def search(request):
+    return render(request, 'articles/search.html')
+
+def search_result(request):
+    request_data = request.GET
+    print(request_data)
+    context = {
+        'num': request_data.get('num')
+    }
+    return render(request, 'articles/search_result.html', context)
+
+
+def detail(request, num):
+    context = {
+        'num': num
+    }
+    return render(request, 'articles/detail.html', context)
