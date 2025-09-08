0157a233b99e1dfb43b059996e4d974b597c76d7
taek-99 <gunha1215@gmail.com>
2025-09-08 10:57:57 +0900

    tt

---
+++ TIL/250908/django/06. django_form/day6_django_skeleton/articles/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
+
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/detail.html', context)
+
+def new(request):
+
+    form = ArticleForm()
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'articles/new.html', context)
+
+def create(request):
+    data = request.POST
+    title = data.get('title')
+    content = data.get('content')
+
+    print("title: ", title, content)
+    article = Article.objects.create(title=title, content=content)
+
+    return redirect('articles:detail', article.pk)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+
+    return redirect('articles:index')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article
+    }
+    return render(request, 'articles/edit.html', context)
+
+
+def update(request, pk):
+    data = request.POST
+    title = data.get('title')
+    content = data.get('content')
+
+    article = Article.objects.get(pk=pk)
+    article.title = title
+    article.content =content 
+    article.save()
+    return redirect('articles:detail', article.pk)
+
+
