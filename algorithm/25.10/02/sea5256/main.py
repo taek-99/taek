039b83b3d45dae6b3e26494f9a9e55dd75e60716
taek-99 <gunha1215@gmail.com>
2025-10-02 10:57:58 +0900

    tt

--- algorithm/25.10/02/sea5256/main.py
+++ algorithm/25.10/02/sea5256/main.py
@@ -1,5 +1,17 @@
 import sys
 sys.stdin = open('sample_input (25).txt','r')
 
-n, a, b = map(int, input().split())
+T = int(input())
 
+for tc in range(1, T+1):
+    n, a, b = map(int, input().split())
+    B = [[0 for _ in range(a + 1)] for _ in range(n+1)]
+
+    for i in range(n + 1):
+        for j in range(min(i, a) + 1):
+            if j == 0 or j == i:
+                B[i][j] = 1
+            else:
+                B[i][j] = B[i-1][j-1] + B[i-1][j]
+
+    print(f"#{tc} {B[n][a]}")
