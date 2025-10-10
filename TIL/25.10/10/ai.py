0e452c5e89f28b62285f99a8da09c5233fae84d7
taek-99 <gunha1215@gmail.com>
2025-10-10 17:05:27 +0900

    tt

---
+++ TIL/25.10/10/ai.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+n = 10 ** 10
+np_array = np.arange(n)
+print (np_array)
+
+
+arr = np.array([1, 2, 3, 4,5])
+print (arr)
+
+
+
+
+full_array = np.full((2, 3), 2)
+print (full_array)
