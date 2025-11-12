fef3c113fbfcb1b04c1d7ee2226eb6e8a7b6b25d
taek99 <gunha1215@gmail.com>
2025-11-12 09:05:08 +0900

    tt

---
+++ etc/vue_test/vite-project/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
+</script>
+
+<template>
+
+ <Navbar/>
+</template>
+
+<style>
+
+</style>
